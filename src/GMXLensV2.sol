5dccd350a6cdb447bce741af384ffc8d017dc6a6
alex-posley <alex.tang@posley.capital>
2024-08-16 11:40:57 +0800

    compilation version

--- src/GMXLensV2.sol
+++ src/GMXLensV2.sol
@@ -4,7 +4,18 @@ pragma solidity ^0.8.0;
 import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import { UUPSUpgradeable }from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts/utils/math/SafeCast.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
 import "./MarketDataTypes.sol";
+
+import {IReader} from "./Interfaces/IReader.sol";
+import {IPriceFeed} from "./Interfaces/IPriceFeed.sol";
+import {IDataStore} from "./Interfaces/IDataStore.sol";
+import {IOracle} from "./Interfaces/IOracle.sol";
+import {MarketPoolValueInfo} from "./MarketPoolValueInfo.sol";
+import {Market} from "./Market.sol";
+import {Price} from "./Price.sol";
+import {Keys} from "./Keys.sol";
 
 interface IVault {
     function poolAmounts(address _token) external view returns (uint256);
@@ -32,73 +43,182 @@ interface IPositionStoreUtils {
 }
 
 contract GMXLensV2 is MarketDataTypes, Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    address private orderStoreAddr;
+    address private depositStoreAddr;
+    address private marketStoreAddr;
+    address private positionStoreAddr;
+    address private dataStoreAddr;
+    address private readerAddr;
+    address private oracleAddr;
 
-    function initialize() public initializer {
+    IReader private reader; 
+
+    address immutable _owner;
+
+    function initialize() public initializer  {
         __Ownable_init(msg.sender);
+        orderStoreAddr = 0x97BeB5A20FBd4596c8B19a89Ec399a100e57d14d;
+        depositStoreAddr = 0x98e86155abf8bCbA566b4a909be8cF4e3F227FAf;
+        marketStoreAddr = 0x5a1344252f0CdfDB765DD5ab97C98734f1D7ED6d;
+        positionStoreAddr = 0x4a57C9b3d6c96954e397Cc186F98fCD2816A95C7;
+        readerAddr = 0xdA5A70c885187DaA71E7553ca9F728464af8d2ad;
+        dataStoreAddr = 0xFD70de6b91282D8017aA4E741e9Ae325CAb992d8;
+        oracleAddr = 0xa11B501c2dd83Acd29F6727570f2502FAaa617F2;
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
     function getMarketData(address marketID, address _vault) external view returns (MarketDataState memory) {
         // Interface for the GMX Market Data contract
-        IVault vault = IVault(_vault);
-        IOrderStoreUtils orderStoreUtils = IOrderStoreUtils(0x97BeB5A20FBd4596c8B19a89Ec399a100e57d14d);
-        IDepositStoreUtils depositStoreUtils = IDepositStoreUtils(0x98e86155abf8bCbA566b4a909be8cF4e3F227FAf);
-        IMarketStoreUtils marketStoreUtils = IMarketStoreUtils(0x5a1344252f0CdfDB765DD5ab97C98734f1D7ED6d);
-        IPositionStoreUtils positionStoreUtils = IPositionStoreUtils(0x4a57C9b3d6c96954e397Cc186F98fCD2816A95C7);
-
+        IMarketStoreUtils marketStoreUtils = IMarketStoreUtils(marketStoreAddr);
 
         // Fetching data from the market data contract
-        uint256 poolValue = vault.poolAmounts(marketID);
+        address indexTokenAddress = address(uint160(uint256(marketStoreUtils.INDEX_TOKEN())));
 
-        // bytes32 isLongValue = orderStoreUtils.IS_LONG();
-        // address isLongAddress = address(uint160(uint256(isLongValue)));
-
-        // bytes32 initialShortToken = depositStoreUtils.INITIAL_SHORT_TOKEN();
-        // address initialShortTokenAddress = address(uint160(uint256(initialShortToken)));
-
-        // bytes32 indexToken = marketStoreUtils.INDEX_TOKEN();
-        // address indexTokenAddress = address(uint160(uint256(indexToken)));
-
-        // bytes32 longToken = marketStoreUtils.LONG_TOKEN();
-        // address longTokenAddress = address(uint160(uint256(longToken)));
+        address longTokenAddress = address(uint160(uint256(marketStoreUtils.LONG_TOKEN())));
         
-        // bytes32 shortToken = marketStoreUtils.SHORT_TOKEN();
-        // address shortTokennAddress = address(uint160(uint256(shortToken)));
+        address shortTokenAddress = address(uint160(uint256(marketStoreUtils.SHORT_TOKEN())));
         
         // bytes32 borrowingFactor = positionStoreUtils.BORROWING_FACTOR();
         // address borrowingFactorAddress = address(uint160(uint256(borrowingFactor)));
 
+        Market.Props memory marketProps = reader.getMarket(dataStoreAddr, marketID);
+        Price.MarketPrices memory marketPrices = Price.MarketPrices(getTokenPrice(indexTokenAddress), getTokenPrice(longTokenAddress), getTokenPrice(shortTokenAddress));
 
-        // Constructing the MarketDataState struct to return, this is dummy data
-        MarketDataState memory state = MarketDataState({
-            marketToken: marketID,
-                indexToken: address(2),
-                longToken: address(3),
-                shortToken: address(4),
-                poolValue: poolValue,
-                longTokenAmount: 500,
-                longTokenUsd: 600,
-                shortTokenAmount: 400,
-                shortTokenUsd: 300,
-                openInterestLong: 200,
-                openInterestShort: -100,
-                pnlLong: 50,
-                pnlShort: -50,
-                netPnl: 0,
-                borrowingFactorPerSecondForLongs: 1e18,
-                borrowingFactorPerSecondForShorts: 1e18,
-                longsPayShorts: true,
-                fundingFactorPerSecond: 1e18,
-                fundingFactorPerSecondLongs: 1e18,
-                fundingFactorPerSecondShorts: -1e18,
-                reservedUsdLong: 100,
-                reservedUsdShort: 100,
-                maxOpenInterestUsdLong: 10000,
-                maxOpenInterestUsdShort: 10000
-        });
+        (,MarketPoolValueInfo.Props memory marketPoolValueInfo) = reader
+            .getMarketTokenPrice(
+                dataStoreAddr,
+                marketProps,
+                marketPrices.indexTokenPrice,
+                marketPrices.longTokenPrice,
+                marketPrices.shortTokenPrice,
+                Keys.MAX_PNL_FACTOR_FOR_TRADERS,
+                true
+            );
+        {
+            MarketDataState memory state = MarketDataState({
+                marketToken: marketID,
+                indexToken: indexTokenAddress,
+                longToken: longTokenAddress,
+                shortToken: shortTokenAddress,
+                poolValue: marketPoolValueInfo.poolValue,
+                longTokenAmount: marketPoolValueInfo.longTokenAmount,
+                longTokenUsd: marketPoolValueInfo.longTokenUsd,
+                shortTokenAmount: marketPoolValueInfo.shortTokenAmount,
+                shortTokenUsd: marketPoolValueInfo.shortTokenUsd,
+                openInterestLong: getOpenInterest(marketProps, true),
+                openInterestShort: getOpenInterest(marketProps, false),
+                pnlLong: reader.getPnl(dataStoreAddr, marketProps, marketPrices.indexTokenPrice, true, false),
+                pnlShort: reader.getPnl(dataStoreAddr, marketProps, marketPrices.indexTokenPrice, false, false),
+                netPnl: reader.getNetPnl(dataStoreAddr, marketProps, marketPrices.indexTokenPrice, false)
+                // borrowingFactorPerSecondForLongs: 1e18,
+                // borrowingFactorPerSecondForShorts: 1e18,
+                // longsPayShorts: true,
+                // fundingFactorPerSecond: 1e18,
+                // fundingFactorPerSecondLongs: 1e18,
+                // fundingFactorPerSecondShorts: -1e18,
+                // reservedUsdLong: 100,
+                // reservedUsdShort: 100,
+                // maxOpenInterestUsdLong: 10000,
+                // maxOpenInterestUsdShort: 10000
+            });
 
-        return state;
+            return state;
+        }
+    }
+
+    /** @dev the long and short open interest for a market based on the collateral token used
+        @param market the market to check
+        @param collateralToken the collateral token to check
+        @param isLong whether to check the long or short side
+        @param divisor divisor for market
+    */
+    function getOpenInterest(
+        address market,
+        address collateralToken,
+        bool isLong,
+        uint256 divisor
+    ) internal view returns (uint256) {
+        return
+            IDataStore(dataStoreAddr).getUint(
+                Keys.openInterestKey(market, collateralToken, isLong)
+            ) / divisor;
+    }
+
+    /** @dev get either the long or short open interest for a market
+     *  @param market the market to check
+     *  @param isLong whether to get the long or short open interest
+     *  @return the long or short open interest for a market
+     */
+    function getOpenInterest(
+        Market.Props memory market,
+        bool isLong
+    ) internal view returns (uint256) {
+        uint256 divisor = market.longToken == market.shortToken ? 2 : 1;
+        uint256 openInterestUsingLongTokenAsCollateral = getOpenInterest(
+            market.marketToken,
+            market.longToken,
+            isLong,
+            divisor
+        );
+        uint256 openInterestUsingShortTokenAsCollateral = getOpenInterest(
+            market.marketToken,
+            market.shortToken,
+            isLong,
+            divisor
+        );
+
+        return
+            openInterestUsingLongTokenAsCollateral +
+            openInterestUsingShortTokenAsCollateral;
+    }
+
+    /** @dev get the multiplier value to convert the external price feed price to the price of 1 unit of the token
+    represented with 30 decimals
+    @param token token to get price feed multiplier for
+    */
+    function getPriceFeedMultiplier(
+        address token
+    ) public view returns (uint256) {
+        uint256 multiplier = IDataStore(dataStoreAddr).getUint(
+            Keys.priceFeedMultiplierKey(token)
+        );
+
+        return multiplier;
+    }
+
+
+    /** @dev get the token price by fetching token price from token's price feed address in 30 decimals
+        @param token token to get price feed multiplier 
+    */
+    function getTokenPrice(
+        address token
+    ) internal view returns (Price.Props memory) {
+        IPriceFeed priceFeed = IPriceFeed(
+            IDataStore(dataStoreAddr).getAddress(Keys.priceFeedKey(token))
+        );
+
+        if (address(priceFeed) == address(0)) {
+            Price.Props memory primaryPrice = IOracle(oracleAddr).primaryPrices(
+                token
+            );
+            require(
+                primaryPrice.min != 0 && primaryPrice.max != 0,
+                "Not able to fetch latest price"
+            );
+            return primaryPrice;
+        }
+
+        uint256 multiplier = getPriceFeedMultiplier(token);
+
+        (, int256 tokenPrice, , , ) = priceFeed.latestRoundData();
+
+        uint256 price = Math.mulDiv(
+            SafeCast.toUint256(tokenPrice),
+            multiplier,
+            10 ** 30
+        );
+        return Price.Props(price, price);
     }
 
 }
--- src/MarketDataTypes.sol
+++ src/MarketDataTypes.sol
@@ -7,25 +7,26 @@ contract MarketDataTypes {
         address indexToken;
         address longToken;
         address shortToken;
-        uint256 poolValue;
+        int256 poolValue;
         uint256 longTokenAmount;
         uint256 longTokenUsd;
         uint256 shortTokenAmount;
         uint256 shortTokenUsd;
-        int256 openInterestLong;
-        int256 openInterestShort;
+        uint256 openInterestLong;
+        uint256 openInterestShort;
         int256 pnlLong;
         int256 pnlShort;
         int256 netPnl;
-        uint256 borrowingFactorPerSecondForLongs;
-        uint256 borrowingFactorPerSecondForShorts;
-        bool longsPayShorts;
-        uint256 fundingFactorPerSecond;
-        int256 fundingFactorPerSecondLongs;
-        int256 fundingFactorPerSecondShorts;
-        uint256 reservedUsdLong;
-        uint256 reservedUsdShort;
-        uint256 maxOpenInterestUsdLong;
-        uint256 maxOpenInterestUsdShort;
+
+        // uint256 borrowingFactorPerSecondForLongs;
+        // uint256 borrowingFactorPerSecondForShorts;
+        // bool longsPayShorts;
+        // uint256 fundingFactorPerSecond;
+        // int256 fundingFactorPerSecondLongs;
+        // int256 fundingFactorPerSecondShorts;
+        // uint256 reservedUsdLong;
+        // uint256 reservedUsdShort;
+        // uint256 maxOpenInterestUsdLong;
+        // uint256 maxOpenInterestUsdShort;
     }
 }
